a5a8ade2ae555222c99d972edc8ca3b85ceeed31
jeronima <jeronima.floriano@totvs.com.br>
2023-02-06 14:19:04 -0300

    Criando o subscriber para a fila de emissao de cartoes

--- cartoes/src/main/java/io/github/starbank/cartoes/CartoesApplication.java
+++ cartoes/src/main/java/io/github/starbank/cartoes/CartoesApplication.java
@@ -3,9 +3,11 @@ package io.github.starbank.cartoes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableRabbit
 public class CartoesApplication {
 
 	public static void main(String[] args) {
